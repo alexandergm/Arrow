69d8ea1b080371a8dcddfde9e78d3b3bbf49d805
alexandergm <sasha_261@outlook.com>
2020-08-09 00:42:34 +0300

    Initial commit

---
+++ Assets/Anima/NewBehaviourScript.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewBehaviourScript : MonoBehaviour
+{
+
+    
+    public GameObject camera;
+
+
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+    void OnTriggerEnter(Collider col)
+    {
+        if (col.gameObject.CompareTag("Player"))
+        {
+           
+            camera.GetComponent<CameraControl>().enabled = false;
+
+        }
+    }
+}
---
+++ Assets/Anima/SimplePlayer.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SimplePlayer : MonoBehaviour
+{
+    
+    public GameObject obj;
+    public GameObject rt;
+    public GameObject camera;
+
+
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+    void OnTriggerEnter(Collider col)
+    {
+        if (col.gameObject.CompareTag("Player"))
+        {
+            print(obj.gameObject.name);
+            obj.GetComponent<Animator>().SetBool("Bool", true);
+
+            //rt.GetComponent<MeshRenderer>().enabled = false;
+            //camera.GetComponent<CameraControl>().enabled = false;
+            Destroy(rt);            
+        }
+    }
+}
