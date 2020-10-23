c2f3b0e7b9f830cdfe7a6b7df3c096687818d4fd
alexandergm <sasha_261@outlook.com>
2020-10-23 13:15:28 +0300

    Put the project in order

---
+++ Assets/OURProject/Script/Coin.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class Coin : MonoBehaviour
+{
+
+    public GameObject obj;
+    //public GameObject starLeft;
+    //public GameObject text;
+    public ParticleSystem particleLauncher;
+    public ParticleSystem particleLauncher1;
+    public ParticleSystem particleLauncher2;
+    public ParticleSystem particleLauncher3;
+
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+    void OnTriggerEnter(Collider col)
+    {
+        if (col.gameObject.CompareTag("Player"))
+        {
+            //text.GetComponent<Text>().text = "100";
+            particleLauncher.Emit(1);
+            particleLauncher1.Emit(1);
+            particleLauncher2.Emit(1);
+            particleLauncher3.Emit(1);
+            //starLeft.SetActive(true);
+            
+            Destroy(obj);
+        }
+    }
+
+}
